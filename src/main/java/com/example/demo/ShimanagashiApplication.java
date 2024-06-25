79a80d14bf2b67ff7d5792b89540b2d2ea3bf6da
s-umemura <22110036@oic-ok.ac.jp>
2024-06-25 11:26:55 +0900

    first

---
+++ src/main/java/com/example/demo/ShimanagashiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShimanagashiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShimanagashiApplication.class, args);
+	}
+
+}
